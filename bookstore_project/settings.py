28b71f0ce9d9fc206e4e07ba714ba1cb284d75de
Masahiro Uchiyama <uchiyama30.04@gmail.com>
2020-06-22 01:04:47 +0900

    ch9

--- bookstore_project/settings.py
+++ bookstore_project/settings.py
@@ -156,7 +156,7 @@ AUTHENTICATION_BACKENDS = (
     'allauth.account.auth_backends.AuthenticationBackend',
 )
 
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.consolsmtp.EmailBackend'
 
 ACCOUNT_SESSION_REMEMBER = True
 ACCOUNT_SIGNUP_PASSWORD_ENTER_TWICE = False
@@ -164,3 +164,5 @@ ACCOUNT_USERNAME_REQUIRED = False
 ACCOUNT_AUTHENTICATION_METHOD = 'email'
 ACCOUNT_EMAIL_REQUIRED = True
 ACCOUNT_UNIQURE_EMAIL =True
+
+DEFAULT_FROM_EMAIL = 'admin@djangobookstore.com'
