9854ff5e4d8d6a454f60622bd96a3bfab9b046d0
Masahiro Uchiyama <uchiyama30.04@gmail.com>
2020-06-23 08:57:24 +0900

    ch12

--- bookstore_project/settings.py
+++ bookstore_project/settings.py
@@ -140,6 +140,8 @@ STATICFILES_FINDERS = [
     "django.contrib.staticfiles.finders.FileSystemFinder",
     "django.contrib.staticfiles.finders.AppDirectoriesFinder",
 ]
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 AUTH_USER_MODEL = 'users.CustomUser'
 
