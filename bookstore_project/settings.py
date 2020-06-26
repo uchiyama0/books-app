22ca903803d109d9272eafdb18cbe9d0b052403e
Masahiro Uchiyama <uchiyama30.04@gmail.com>
2020-06-27 00:57:42 +0900

    SECURE_REFERRER_POLICY changed

--- bookstore_project/settings.py
+++ bookstore_project/settings.py
@@ -204,7 +204,7 @@ if ENVIRONMENT == 'production':
     SECURE_CONTENT_TYPE_NOSNIFF = True
     SESSION_COOKIE_SECURE = True
     CSRF_COOKIE_SECURE = True
-    SECURE_REFERRER_POLICY = 'no-referrer'
+    SECURE_REFERRER_POLICY = 'same-origin'
     SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
 # Heroku
