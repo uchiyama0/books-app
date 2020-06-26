5ad6ef497cc6581310e6557a5a284e900cf108cb
Masahiro Uchiyama <uchiyama30.04@gmail.com>
2020-06-27 00:33:13 +0900

    ch18_2

--- bookstore_project/settings.py
+++ bookstore_project/settings.py
@@ -209,4 +209,4 @@ if ENVIRONMENT == 'production':
 # Heroku
 import dj_database_url
 db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASE['default'].update(db_from_env)
+DATABASES['default'].update(db_from_env)
